a450e5b4d92fc72643b0e7906e1745381ab7e492
Michael Vastarelli <mike@bitcobblers.com>
2024-01-02 18:58:32 -0500

    Reformatted code.

--- gosu-core/src/main/java/gw/internal/gosu/module/fs/FileSystemImpl.java
+++ gosu-core/src/main/java/gw/internal/gosu/module/fs/FileSystemImpl.java
@@ -31,13 +31,14 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FileSystemImpl extends BaseService implements IFileSystem {
+  private static final Set<String> FILE_SUFFIXES;
 
-  private Map<File, IDirectory> _cachedDirInfo;
+  private final Map<File, IDirectory> _cachedDirInfo;
   private CachingMode _cachingMode;
 
-  private FileSystemImpl.IDirectoryResourceExtractor _iDirectoryResourceExtractor;
-  private FileSystemImpl.IFileResourceExtractor _iFileResourceExtractor;
-  private Map<String, IProtocolAdapter> _protocolAdapters;
+  private final DirectoryResourceExtractor _iDirectoryResourceExtractor;
+  private final FileResourceExtractor _iFileResourceExtractor;
+  private final Map<String, IProtocolAdapter> _protocolAdapters;
 
   public static boolean USE_NEW_API = false;
 
@@ -46,11 +47,37 @@ public class FileSystemImpl extends BaseService implements IFileSystem {
   // turn into a perf issue
   static final Object CACHED_FILE_SYSTEM_LOCK = new Object();
 
+  static {
+    FILE_SUFFIXES = new HashSet<String>();
+    FILE_SUFFIXES.add("class");
+    FILE_SUFFIXES.add("eti");
+    FILE_SUFFIXES.add("etx");
+    FILE_SUFFIXES.add("gif");
+    FILE_SUFFIXES.add("gr");
+    FILE_SUFFIXES.add("grs");
+    FILE_SUFFIXES.add("gs");
+    FILE_SUFFIXES.add("gst");
+    FILE_SUFFIXES.add("gsx");
+    FILE_SUFFIXES.add("gti");
+    FILE_SUFFIXES.add("gx");
+    FILE_SUFFIXES.add("jar");
+    FILE_SUFFIXES.add("java");
+    FILE_SUFFIXES.add("pcf");
+    FILE_SUFFIXES.add("png");
+    FILE_SUFFIXES.add("properties");
+    FILE_SUFFIXES.add("tti");
+    FILE_SUFFIXES.add("ttx");
+    FILE_SUFFIXES.add("txt");
+    FILE_SUFFIXES.add("wsdl");
+    FILE_SUFFIXES.add("xml");
+    FILE_SUFFIXES.add("xsd");
+  }
+
   public FileSystemImpl(CachingMode cachingMode) {
     _cachedDirInfo = new HashMap<>();
     _cachingMode = cachingMode;
-    _iDirectoryResourceExtractor = new IDirectoryResourceExtractor();
-    _iFileResourceExtractor = new IFileResourceExtractor();
+    _iDirectoryResourceExtractor = new DirectoryResourceExtractor();
+    _iFileResourceExtractor = new FileResourceExtractor();
     _protocolAdapters = new ConcurrentHashMap<>();
     loadProtocolAdapters();
   }
@@ -67,7 +94,7 @@ public class FileSystemImpl extends BaseService implements IFileSystem {
     if( !Files.isDirectory( dir ) )
     {
       throw new IllegalArgumentException(
-        "'" + dir + "' is not a directory of the '" + dir.getFileSystem() + "' file system" );
+              "'" + dir + "' is not a directory of the '" + dir.getFileSystem() + "' file system" );
     }
     return new PathDirectoryImpl( dir );
   }
@@ -106,8 +133,9 @@ public class FileSystemImpl extends BaseService implements IFileSystem {
     if( Files.isDirectory( path ) )
     {
       throw new IllegalArgumentException(
-        "'" + path + "' is not a file of the '" + path.getFileSystem() + "' file system" );
+              "'" + path + "' is not a file of the '" + path.getFileSystem() + "' file system" );
     }
+
     return new PathFileImpl( path );
   }
 
@@ -121,94 +149,6 @@ public class FileSystemImpl extends BaseService implements IFileSystem {
       return null;
     } else {
       return new JavaFileImpl( normalizeFile( file ) );
-    }
-  }
-
-  public static File normalizeFile(File file) {
-
-//    return file;
-    String absolutePath = file.getAbsolutePath();
-    List<String> components = new ArrayList<String>();
-
-    boolean reallyNormalized = false;
-    int lastIndex = 0;
-    for (int i = 0; i < absolutePath.length(); i++) {
-      char c = absolutePath.charAt(i);
-      if (c == '/' || c == '\\') {
-        String component = absolutePath.substring(lastIndex, i);
-        if (component.equals(".")) {
-          reallyNormalized = true;
-        } else if (component.equals("..")) {
-          components.remove(components.size() - 1);
-          reallyNormalized = true;
-        } else {
-          components.add(component);
-        }
-        lastIndex = i + 1;
-      }
-    }
-
-    String component = absolutePath.substring(lastIndex);
-    if (component.equals(".")) {
-      reallyNormalized = true;
-    } else if (component.equals("..")) {
-      components.remove(components.size() - 1);
-      reallyNormalized = true;
-    } else {
-      components.add(component);
-    }
-
-    return reallyNormalized ? new File(GosuStringUtil.join(components, "/")) : file;
-  }
-
-  @Override
-  public void setCachingMode(CachingMode cachingMode) {
-    synchronized (CACHED_FILE_SYSTEM_LOCK) {
-      _cachingMode = cachingMode;
-      for (IDirectory dir : _cachedDirInfo.values()) {
-        if (dir instanceof JavaDirectoryImpl) {
-          ((JavaDirectoryImpl) dir).setCachingMode(cachingMode);
-        }
-      }
-    }
-  }
-
-  private IDirectory createDir( File dir ) {
-    // PL-21817 in OSGi/Equinox JAR could be named as "bundlefile"
-    if ( (dir.getName().toLowerCase().endsWith(".jar") || dir.getName().toLowerCase().endsWith(".zip") || dir.getName().equals("bundlefile")) && dir.isFile()) {
-      return new JarFileDirectoryImpl( dir );
-    } else {
-      return new JavaDirectoryImpl( dir, _cachingMode );
-    }
-  }
-
-  public void clearAllCaches() {
-    if (USE_NEW_API) {
-      FileFactory.instance().getDefaultPhysicalFileSystem().clearAllCaches();
-      return;
-    }
-    synchronized (CACHED_FILE_SYSTEM_LOCK) {
-      for (IDirectory dir : _cachedDirInfo.values()) {
-        dir.clearCaches();
-      }
-    }
-  }
-
-  static boolean isDirectory(File f) {
-    String name = f.getName();
-    if (isAssumedFileSuffix(getFileSuffix(name))) {
-      return false;
-    } else {
-      return f.isDirectory();
-    }
-  }
-
-  private static String getFileSuffix(String name) {
-    int dotIndex = name.lastIndexOf('.');
-    if (dotIndex == -1) {
-      return null;
-    } else {
-      return name.substring(dotIndex + 1);
     }
   }
 
@@ -248,6 +188,99 @@ public class FileSystemImpl extends BaseService implements IFileSystem {
     return null;
   }
 
+  @Override
+  public void setCachingMode(CachingMode cachingMode) {
+    synchronized (CACHED_FILE_SYSTEM_LOCK) {
+      _cachingMode = cachingMode;
+
+      for (IDirectory dir : _cachedDirInfo.values()) {
+        if (dir instanceof JavaDirectoryImpl) {
+          ((JavaDirectoryImpl) dir).setCachingMode(cachingMode);
+        }
+      }
+    }
+  }
+
+  public void clearAllCaches() {
+    if (USE_NEW_API) {
+      FileFactory.instance().getDefaultPhysicalFileSystem().clearAllCaches();
+      return;
+    }
+
+    synchronized (CACHED_FILE_SYSTEM_LOCK) {
+      for (IDirectory dir : _cachedDirInfo.values()) {
+        dir.clearCaches();
+      }
+    }
+  }
+
+  public static File normalizeFile(File file) {
+    String absolutePath = file.getAbsolutePath();
+    List<String> components = new ArrayList<String>();
+    boolean reallyNormalized = false;
+    int lastIndex = 0;
+
+    for (int i = 0; i < absolutePath.length(); i++) {
+      char c = absolutePath.charAt(i);
+
+      if (c == '/' || c == '\\') {
+        String component = absolutePath.substring(lastIndex, i);
+
+        if (component.equals(".")) {
+          reallyNormalized = true;
+        } else if (component.equals("..")) {
+          components.remove(components.size() - 1);
+          reallyNormalized = true;
+        } else {
+          components.add(component);
+        }
+
+        lastIndex = i + 1;
+      }
+    }
+
+    String component = absolutePath.substring(lastIndex);
+
+    if (component.equals(".")) {
+      reallyNormalized = true;
+    } else if (component.equals("..")) {
+      components.remove(components.size() - 1);
+      reallyNormalized = true;
+    } else {
+      components.add(component);
+    }
+
+    return reallyNormalized ? new File(GosuStringUtil.join(components, "/")) : file;
+  }
+
+  static boolean isDirectory(File f) {
+    String name = f.getName();
+
+    if (isAssumedFileSuffix(getFileSuffix(name))) {
+      return false;
+    } else {
+      return f.isDirectory();
+    }
+  }
+
+  private static String getFileSuffix(String name) {
+    int dotIndex = name.lastIndexOf('.');
+    if (dotIndex == -1) {
+      return null;
+    } else {
+      return name.substring(dotIndex + 1);
+    }
+  }
+
+  private IDirectory createDir( File dir ) {
+    // PL-21817 in OSGi/Equinox JAR could be named as "bundlefile"
+    if ( (dir.getName().toLowerCase().endsWith(".jar") || dir.getName().toLowerCase().endsWith(".zip") || dir.getName().equals("bundlefile")) && dir.isFile()) {
+      return new JarFileDirectoryImpl( dir );
+    } else {
+      return new JavaDirectoryImpl( dir, _cachingMode );
+    }
+  }
+
   private void loadProtocolAdapters() {
     ServiceLoader<IProtocolAdapter> adapters = ServiceLoader.load(IProtocolAdapter.class, getClass().getClassLoader());
     for (IProtocolAdapter adapter : adapters) {
@@ -257,88 +290,77 @@ public class FileSystemImpl extends BaseService implements IFileSystem {
     }
   }
 
-  private void loadProtocolAdapter(Collection<IProtocolAdapter> adapters, String adapterName) {
-    try {
-      Class<? extends IProtocolAdapter> adapterClass =
-              Class.forName(adapterName, true, Thread.currentThread().getContextClassLoader()).asSubclass(IProtocolAdapter.class);
-      adapters.add(adapterClass.newInstance());
-    } catch (ClassNotFoundException e) {
-      // It's not in the classpath, just ignore
-    } catch (InstantiationException e) {
-      throw new RuntimeException(e);
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private abstract class ResourceExtractor<J extends IResource> {
-
-    J getClassResource(URL _url) {
+  private abstract class ResourceExtractor<TResource extends IResource> {
+    TResource getClassResource(URL _url) {
       if (_url == null) {
         return null;
       }
 
-      if ( _url.getProtocol().equals( "file" ) ) {
-        return getIResourceFromJavaFile(_url);
-      }
-      else if ( _url.getProtocol().equals( "jar" ) ) {
-        JarURLConnection urlConnection;
-        URL jarFileUrl;
-        try {
-          urlConnection = (JarURLConnection) _url.openConnection();
-          jarFileUrl = urlConnection.getJarFileURL();
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-        File dir = new File(jarFileUrl.getFile());
+      switch (_url.getProtocol()) {
+        case "file":
+          return getResourceFromJavaFile(_url);
+        case "jar":
+          JarURLConnection urlConnection;
+          URL jarFileUrl;
 
-        IDirectory jarFileDirectory;
-        synchronized (CACHED_FILE_SYSTEM_LOCK) {
-          jarFileDirectory = _cachedDirInfo.get(dir);
-          if (jarFileDirectory == null) {
-            jarFileDirectory = createDir( dir );
-            _cachedDirInfo.put( dir, jarFileDirectory );
+          try {
+            urlConnection = (JarURLConnection) _url.openConnection();
+            jarFileUrl = urlConnection.getJarFileURL();
+          } catch (IOException e) {
+            throw new RuntimeException(e);
           }
-        }
 
-        return getIResourceFromJarDirectoryAndEntryName(jarFileDirectory,urlConnection.getEntryName());
-      }
-      else if ( _url.getProtocol().equals( "http" ) ) {
-        J res = getIResourceFromURL(_url);
-        if ( res != null ) {
-          return res;
-        }
-      }
-      else
-      {
-        try
-        {
-          // Handle custom file systems e.g., jrt:/
+          File dir = new File(jarFileUrl.getFile());
+          IDirectory jarFileDirectory;
 
-          Path path = Paths.get( _url.toURI() );
-          return getIResourceFromPath( path );
-        }
-        catch( Exception ignore )
-        {
-        }
+          synchronized (CACHED_FILE_SYSTEM_LOCK) {
+            jarFileDirectory = _cachedDirInfo.get(dir);
+
+            if (jarFileDirectory == null) {
+              jarFileDirectory = createDir(dir);
+              _cachedDirInfo.put(dir, jarFileDirectory);
+            }
+          }
+
+          return getResourceFromJarDirectoryAndEntryName(jarFileDirectory, urlConnection.getEntryName());
+        case "http":
+          TResource res = getResourceFromURL(_url);
+
+          if (res != null) {
+            return res;
+          }
+
+          break;
+        default:
+          try {
+            // Handle custom file systems e.g., jrt:/
+            Path path = Paths.get(_url.toURI());
+            return getResourceFromPath(path);
+          } catch (Exception ignore) {
+          }
+
+          break;
       }
+
       throw new RuntimeException( "Unrecognized protocol: " + _url.getProtocol() );
     }
 
-    abstract J getIResourceFromURL(URL location);
+    abstract TResource getResourceFromURL(URL location);
 
-    abstract J getIResourceFromJarDirectoryAndEntryName(IDirectory jarFS, String entryName);
+    abstract TResource getResourceFromJarDirectoryAndEntryName(IDirectory jarFS, String entryName);
 
-    abstract J getIResourceFromJavaFile(URL location);
+    abstract TResource getResourceFromJavaFile(URL location);
 
-    abstract J getIResourceFromPath(Path path);
+    abstract TResource getResourceFromPath(Path path);
 
     protected File getFileFromURL(URL url) {
       try {
         URI uri = url.toURI();
+
         if ( uri.getFragment() != null ) {
           uri = new URI( uri.getScheme(), uri.getSchemeSpecificPart(), null );
         }
+
         return new File( uri );
       }
       catch ( URISyntaxException ex ) {
@@ -349,83 +371,53 @@ public class FileSystemImpl extends BaseService implements IFileSystem {
         throw new IllegalArgumentException( "Unable to parse URL " + url.toExternalForm(), ex );
       }
     }
-
   }
 
-  private class IFileResourceExtractor extends ResourceExtractor<IFile> {
+  private class FileResourceExtractor extends ResourceExtractor<IFile> {
 
-    IFile getIResourceFromJarDirectoryAndEntryName(IDirectory jarFS, String entryName) {
+    IFile getResourceFromJarDirectoryAndEntryName(IDirectory jarFS, String entryName) {
       return jarFS.file(entryName);
     }
 
-    IFile getIResourceFromJavaFile(URL location) {
+    IFile getResourceFromJavaFile(URL location) {
       return CommonServices.INSTANCE.getFileSystem().getIFile( getFileFromURL(location) );
     }
 
     @Override
-    IFile getIResourceFromURL(URL location) {
+    IFile getResourceFromURL(URL location) {
       return new URLFileImpl(location);
     }
 
     @Override
-    IFile getIResourceFromPath( Path path )
+    IFile getResourceFromPath(Path path )
     {
       return new PathFileImpl( path );
     }
   }
 
-  private class IDirectoryResourceExtractor extends ResourceExtractor<IDirectory> {
+  private class DirectoryResourceExtractor extends ResourceExtractor<IDirectory> {
 
-    protected IDirectory getIResourceFromJarDirectoryAndEntryName(IDirectory dir, String entryName) {
+    protected IDirectory getResourceFromJarDirectoryAndEntryName(IDirectory dir, String entryName) {
       return dir.dir(entryName);
     }
 
-    protected IDirectory getIResourceFromJavaFile(URL location) {
+    protected IDirectory getResourceFromJavaFile(URL location) {
       return CommonServices.INSTANCE.getFileSystem().getIDirectory( getFileFromURL(location) );
     }
 
     @Override
-    IDirectory getIResourceFromURL(URL location) {
+    IDirectory getResourceFromURL(URL location) {
       return null;
     }
 
     @Override
-    IDirectory getIResourceFromPath( Path path )
+    IDirectory getResourceFromPath(Path path )
     {
       return new PathDirectoryImpl( path );
     }
   }
 
-  private static final Set<String> FILE_SUFFIXES;
-
-  static {
-    FILE_SUFFIXES = new HashSet<String>();
-    FILE_SUFFIXES.add("class");
-    FILE_SUFFIXES.add("eti");
-    FILE_SUFFIXES.add("etx");
-    FILE_SUFFIXES.add("gif");
-    FILE_SUFFIXES.add("gr");
-    FILE_SUFFIXES.add("grs");
-    FILE_SUFFIXES.add("gs");
-    FILE_SUFFIXES.add("gst");
-    FILE_SUFFIXES.add("gsx");
-    FILE_SUFFIXES.add("gti");
-    FILE_SUFFIXES.add("gx");
-    FILE_SUFFIXES.add("jar");
-    FILE_SUFFIXES.add("java");
-    FILE_SUFFIXES.add("pcf");
-    FILE_SUFFIXES.add("png");
-    FILE_SUFFIXES.add("properties");
-    FILE_SUFFIXES.add("tti");
-    FILE_SUFFIXES.add("ttx");
-    FILE_SUFFIXES.add("txt");
-    FILE_SUFFIXES.add("wsdl");
-    FILE_SUFFIXES.add("xml");
-    FILE_SUFFIXES.add("xsd");
-  }
-
   private static boolean isAssumedFileSuffix(String suffix) {
     return FILE_SUFFIXES.contains(suffix);
   }
-
 }
